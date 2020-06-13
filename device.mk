82ae9cd79bcf64c560476ddd3efc47eaf8bee804
Jesse Chan <jc@lineageos.org>
2020-06-13 22:45:25 +0800

    denniz: Enable unspecialized app process pool
    Change-Id: I6dfcb59dace5eaa7551caeae86db3b485c0cfbf1
    Signed-off-by: Jesse Chan <jc@lineageos.org>
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>

--- device.mk
+++ device.mk
@@ -72,6 +72,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
 
+# System prop
+-include $(DEVICE_PATH)/system_prop.mk
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
