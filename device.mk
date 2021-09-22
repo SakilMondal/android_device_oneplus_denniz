09517e8e102b2c4cce9b5f7cf09073895166d6d7
ManshuTyagi <himanshut567@gmail.com>
2021-09-22 11:52:35 +0530

    denniz: overlay: Import Overlay from stock
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Change-Id: I0a81ce1b97305d7b852bd0b57fe7cf77b0646ccb

--- device.mk
+++ device.mk
@@ -59,6 +59,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.denniz
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
