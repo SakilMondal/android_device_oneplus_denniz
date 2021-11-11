b48c74e79d356fceb8ea117189166194ee495167
Kshitij Gupta <kshitijgm@gmail.com>
2021-11-12 02:48:01 +0530

    denniz: Address missing ImsServiceBase class * Android 11 has deprecated this class. * This is needed by Android 10 IMS stack. * Convert this into a boot JAR that will be available to system globally.
    Co-authored-by: Gagan Malvi <malvi@aospa.co>
    Signed-off-by: Gagan Malvi <malvi@aospa.co>
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
    Change-Id: Ia685e2e10650c34b413c86fb19dd845baa8fa6c8

--- device.mk
+++ device.mk
@@ -119,6 +119,10 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/vendor_overlay/,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/30/)
 
+# Symbols
+PRODUCT_PACKAGES += \
+    libshim_vtservice
+
 # Telephony
 PRODUCT_BOOT_JARS += \
     mediatek-common \
@@ -128,6 +132,9 @@ PRODUCT_BOOT_JARS += \
     mediatek-telecom-common \
     mediatek-telephony-base \
     mediatek-telephony-common
+
+PRODUCT_PACKAGES += \
+    ImsServiceBase
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
