d5cd4bf067ecef65eb503729625b73c73d9cea7a
Nishant Kumar <www.rajsonu13@gmail.com>
2022-02-20 09:08:08 +0530

    denniz:Properties: Minor changes
    * Change PRODUCT_SYSTEM_DEFAULT_PROPERTIES to PRODUCT_SYSTEM_PROPERTIES as it is highly discouraged and will be deprecated by google
    
    * Change PRODUCT_PROPERTY_OVERRIDES to PRODUCT_PRODUCT_PROPERTIES as init reads /product/build.prop after /vendor/build.prop so we can override props set by /vendor/build.prop by specifying props in /product/build.prop
    
    Reference: - https://android.googlesource.com/platform/build/+/master/Changes.md#changes-in-system-properties-settings
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>
    Change-Id: I05825edaa590fea861aa1c7521a07644228326af

--- system_prop.mk
+++ system_prop.mk
@@ -1,13 +1,13 @@
 # Apex
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     ro.apex.updatable=false
 
 # Bluetooth
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     persist.bluetooth.bluetooth_audio_hal.disabled=true
 
 # Camera
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_SYSTEM_PROPERTIES += \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.google.camera \
     vendor.camera.aux.packageblacklist=org.telegram.messenger,com.microsoft.teams,com.discord
 
@@ -17,25 +17,25 @@ PRODUCT_SYSTEM_PROPERTIES += \
     pm.dexopt.bg-dexopt=everything
 
 # GBoard
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.ime.kb_pad_port_b=1 \
     ro.com.google.ime.corner_key_r=35
 
 # IMS
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_SYSTEM_PROPERTIES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=0 \
     persist.vendor.vilte_support=0 \
     persist.vendor.mtk.vilte.enable=0 
 
 # OMX
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_SYSTEM_PROPERTIES += \
     debug.stagefright.omx_default_rank.sw-audio=1 \
     debug.stagefright.omx_default_rank=0 \
     debug.stagefright.ccodec=0
 
 # Performance
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_SYSTEM_PROPERTIES += \
     ro.mtk_perf_simple_start_win=1 \
     ro.mtk_perf_fast_start_win=1 \
     ro.mtk_perf_response_time=1
@@ -45,24 +45,24 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.control_privapp_permissions=log
 
 # Surfaceflinger
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_SYSTEM_PROPERTIES += \
     debug.sf.latch_unsignaled=1 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
 # Zygote
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
 
 # ZRAM
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_SYSTEM_PROPERTIES += \
     ro.zram.mark_idle_delay_mins=60 \
     ro.zram.first_wb_delay_mins=180 \
     ro.zram.periodic_wb_delay_hours=24
 
 # Encryption
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     ro.crypto.dm_default_key.options_format.version=2 \
     ro.crypto.volume.filenames_mode=aes-256-cts \
     ro.crypto.volume.metadata.method=dm-default-key \
