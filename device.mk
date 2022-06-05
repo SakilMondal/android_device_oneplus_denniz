aadityazeo/priv_denniz

denniz: Import audio_policy_configuration from stock
Signed-off-by: ManshuTyagi <himanshut567@gmail.com>
Change-Id: Iaf127c8fa44bfb74e58272824affe88aff8e0fdf

device.mk
@@ -26,6 +26,13 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PLATFORM_VNDK_VERSION)/etc/audio_policy_configuration.xml
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd

SakilMondal/android_device_oneplus_denniz

device.mk
@@ -32,6 +32,13 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PLATFORM_VNDK_VERSION)/etc/audio_policy_configuration.xml
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd