aadityazeo/priv_denniz

denniz: keylayout: Import custom touchpanel keylayout
* Ref: https://github.com/OnePlusOSS/android_kernel_oneplus_mt6893/blob/oneplus/MT6893_R_11.0/drivers/input/touchscreen/oplus_touchscreen/touchpanel_common_driver.c#L472

Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
Change-Id: Ib0cb81834de4d1d7b9c125531e5b49c8bfbcf04e

device.mk
@@ -32,6 +32,7 @@ PRODUCT_PACKAGES += \
 
 # Input
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/touchpanel.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/touchpanel.kl \
     $(LOCAL_PATH)/keylayout/mt63xx-accdet_Headset.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/mt63xx-accdet_Headset.kl
 
 # Soong namespaces

SakilMondal/android_device_oneplus_denniz

device.mk
@@ -114,6 +114,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
 # Input
 PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/keylayout/touchpanel.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/touchpanel.kl \
     $(DEVICE_PATH)/keylayout/mt63xx-accdet_Headset.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/mt63xx-accdet_Headset.kl
 
 # Lights