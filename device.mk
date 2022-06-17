aadityazeo/priv_denniz

denniz: keylayout: Import keylayout from A.07
Change-Id: I0f9d7a7d06edcbe1c86515a9a6ebb67cc6844c51

device.mk
@@ -30,6 +30,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/mt63xx-accdet_Headset.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/mt63xx-accdet_Headset.kl
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)

SakilMondal/android_device_oneplus_denniz

device.mk
@@ -112,6 +112,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 
+# Input
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/keylayout/mt63xx-accdet_Headset.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/mt63xx-accdet_Headset.kl
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.denniz