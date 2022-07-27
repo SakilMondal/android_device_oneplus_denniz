2f9ee739d4ba92fa9077d68abf398818704a21f9
SakilMondal <mondalsakil@gmail.com>
2022-07-27 16:37:11 +0000

    Revert "demmiz: Arrow bringup"
    This reverts commit 93353c99712d513185db1d33cc3e934734d89fdd.

--- aosp_denniz.mk
+++ aosp_denniz.mk
@@ -21,18 +21,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile
 $(call inherit-product, device/oneplus/denniz/device.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Pixel-Experience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_denniz
+PRODUCT_NAME := aosp_denniz
 PRODUCT_DEVICE := denniz
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := DN2101
 PRODUCT_MANUFACTURER := OnePlus
 
+# Gapps
+TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
-
+TARGET_SUPPORTS_QUICK_TAP := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
